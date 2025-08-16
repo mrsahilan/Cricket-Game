e799bea18fb3c758959bd9283e4be974204b7755
Sahil <Sahil@Sanis>
2025-08-16 15:43:02 +0530

    TL Commit

---
+++ Cricket-Game-Project/src/main/java/com/nit/CricketGameProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketGameProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketGameProjectApplication.class, args);
+	}
+
+}
