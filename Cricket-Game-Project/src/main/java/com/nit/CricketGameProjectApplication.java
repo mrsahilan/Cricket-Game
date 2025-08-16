6b2e46222a4d1eb7866a283620e717ced57d4624
Sahil <Sahil@Sanis>
2025-08-16 16:47:19 +0530

    Developer commit

--- Cricket-Game-Project/src/main/java/com/nit/CricketGameProjectApplication.java
+++ Cricket-Game-Project/src/main/java/com/nit/CricketGameProjectApplication.java
@@ -2,12 +2,19 @@ package com.nit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.nit.entity.Player;
 
 @SpringBootApplication
 public class CricketGameProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CricketGameProjectApplication.class, args);
+		ApplicationContext ctx =  SpringApplication.run(CricketGameProjectApplication.class, args);
+		
+		Player player = ctx.getBean(Player.class);
+		Player details = player.getPlayerDetils();
+		System.out.println(details.getId() + " : " + details.getName() + " : " + details.getJearsyNumber());
 	}
 
 }
